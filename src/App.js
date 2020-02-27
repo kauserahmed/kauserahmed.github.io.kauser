4835a7c36920d856eba20e3582066d517d2fb8ab
kauserahmed <54607183+kauserahmed@users.noreply.github.com>
2020-02-27 18:11:49 -0500

    Update App.js
    I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check Me Out!
         </a>
       </header>
     </div>
