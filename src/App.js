9754864500c59fba7bef208f9b1e16f3f8cc84b2
kauserahmed <54607183+kauserahmed@users.noreply.github.com>
2020-04-19 12:57:07 -0400

    setup all the page using react router dom

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-//import Main from './components/main';
-import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
-    <Layout>
-        <Header title="Title" scroll>
+    <Layout style={{background: 'url(https://www.technojobs.co.uk/info/sites/www.technojobs.co.uk/files/styles/article_image_large/public/developer.jpg?itok=MTr0-Po-) center / cover'}}>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Home</Link>} scroll>
             <Navigation>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
-        <Drawer title="Title">
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Home</Link>}>
             <Navigation>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
+              <Link to="/resume">Resume</Link>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
             </Navigation>
         </Drawer>
         <Content>
             <div className="page-content" />
+            <Main/>
         </Content>
     </Layout>
 </div>
-
 
     );
   }
