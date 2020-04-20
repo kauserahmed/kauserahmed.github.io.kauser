8ba190fc800faed1f380da2affc2a29a92d247e2
kauserahmed <54607183+kauserahmed@users.noreply.github.com>
2020-04-19 22:01:14 -0400

    add a profile pic, name, job title, bio with address, phone, email.

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,9 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import profilepic from './kauser.jpg';
+
 
 class Resume extends Component {
     render () {
         return ( 
-            <div><h1>Resume page</h1></div>
+            <div>
+        <Grid>
+          <Cell col={4}>
+            <div style={{textAlign: 'center'}}>
+              <img
+              src={profilepic}
+                alt="avatar"
+                style={{height: '300px'}}
+                 />
+            </div>
+
+            <h2 style={{paddingTop: '2em'}}>Kauser Ahmed</h2>
+            <h4 style={{color: 'grey'}}>Software Developer</h4>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <p> Engaged, 
+             energetic, creative computer science experience. Seeking to leverage 
+             exemplary leadership skills, analytical skills, and excellent coding 
+             skills. Computer expertise in front desk, cashier, troubleshooting, 
+             fixing computer, internet troubleshooting, and helping both employee 
+             and customer with technical issue.</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <h5>Address</h5>
+            <p>3150 Decatur ave Bronx, NY 10467</p>
+            <h5>Phone</h5>
+            <p>(917) 946 6264 </p>
+            <h5>Email</h5>
+            <p>kauserahmed97@gmail.com</p>
+            <h5>Web</h5>
+            <p>Coming soon</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+          </Cell>
+          </Grid>
+          </div>
         )
     }
 }
