8f4bb8203402ea87d831aa8643674308c7eb63ac
kauserahmed <54607183+kauserahmed@users.noreply.github.com>
2020-04-20 03:02:34 -0400

    add my github project link in project.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,11 +1,83 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component {
-    render () {
-        return ( 
-            <div><h1>Projects page</h1></div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+
+    if(this.state.activeTab === 0){
+      return(
+        <div className="projects-grid">
+          {/* Project 1 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <CardTitle style={{color: 'blue', height: '176px', background: 
+          'url() center / cover'}} >My Portfolio</CardTitle>
+            <CardText>
+              This is my Portfolio WebSites. Please Click the GitHub link 
+              below it will take to my GitHub repo.
+            </CardText>
+            <CardActions border>
+            <Button colored href="https://github.com/kauserahmed/kauserahmed.github.io">Github</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+            </CardMenu>
+          </Card>
+
+          {/* Project 2 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: 'blue', height: '176px', background: 'url() center / cover'}} >Design Pad</CardTitle>
+            <CardText>
+            This is my Design Pad project. This project will drag and drop color from one pad to 
+            another pad. Please Click the GitHub link below it will take to my GitHub repo.
+            </CardText>
+            <CardActions border>
+              <Button colored href = "https://github.com/kauserahmed/design-pad">GitHub</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+            </CardMenu>
+          </Card>
+
+          {/* Project 3 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: 'blue', height: '176px', background: 'url() center / cover'}} >Fave Link</CardTitle>
+            <CardText>
+              This is my Fav Link project. You can add name and Link and it will connect you 
+              to the link. Also you can delete the link from the list. Please Click the GitHub link below it will take to my GitHub repo.
+            </CardText>
+            <CardActions border>
+              <Button colored href = "https://github.com/kauserahmed/FavLink/tree/master/web/FavLink">GitHub</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+            </CardMenu>
+          </Card>
+        </div>
+
+
+      )
+      
+    } 
+
+  }
+
+  render() {
+    return(
+      <div>
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+          <Tab>GitHub Projects</Tab>  
+        </Tabs>
+
+          <Grid>
+            <Cell col={12}>
+              <div className="content">{this.toggleCategories()}</div>
+            </Cell>
+          </Grid>
+      </div>
+    )
+  }
 }
 
-export default Projects ;
+export default Projects;
