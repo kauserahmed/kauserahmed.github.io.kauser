daa53e8af96bdb50b4efee686675875041138931
kauserahmed <54607183+kauserahmed@users.noreply.github.com>
2020-04-19 20:41:15 -0400

    add my bio in about me

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -3,7 +3,14 @@ import React, {Component} from 'react';
 class About extends Component {
     render () {
         return ( 
-            <div><h1>About page</h1></div>
+            <div> <h1 style={{ width: '20%', margin: 'auto', paddingTop: '0em'}}>About Me</h1>
+      <h3 style={{ width: '70%', margin: 'auto', paddingTop: '2em'}}>
+      Hello! I'm Kauser Ahmed, a software developer based of New York. 
+      I enjoy building things for people. I have build Andriod Apps, Websites, 
+      and other software. Currently I'm studying at City universtiy in New York.
+       After graduating from college, I'm interesting to do more daily basis 
+       project. I'm also interesting in AI. Currently I'm self studying about AI.
+        In the future I want to build something that will help others. </h3> </div>
         )
     }
 }
