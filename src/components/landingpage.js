e8b8cec0d9d1d5415ccac4c55c1c0e20aeba3c59
kauserahmed <54607183+kauserahmed@users.noreply.github.com>
2020-04-19 20:23:13 -0400

    add profile pic, name, andjob title in home page.

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,11 +1,35 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import profilepic from './kauser.jpg'
+
 
 class Landing extends Component {
-    render () {
-        return ( 
-            <div><h1>Landing page</h1></div>
-        )
-    }
+  render() {
+    return(
+      <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src={profilepic}
+              
+              alt="Profilepic"
+              className="avatar-img"
+              />
+
+            <div className="banner-text">
+              <h1>Kauser Ahmed</h1>
+           
+            <hr/>
+
+          <p>Software Developer</p>
+
+        
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
 }
 
 export default Landing;
